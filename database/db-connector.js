33aacfd7e966b52977a2640d909f03319b896831
malikhum <malikhum@oregonstate.edu>
2024-03-18 16:04:11 -0700

    sanatized code

--- database/db-connector.js
+++ database/db-connector.js
@@ -15,9 +15,9 @@ var mysql = require('mysql')
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_malikhum',
-    password        : '2527',
-    database        : 'cs340_malikhum'
+    user            : '*',
+    password        : '*',
+    database        : '*'
 })
 
 // Export it for use in our application
